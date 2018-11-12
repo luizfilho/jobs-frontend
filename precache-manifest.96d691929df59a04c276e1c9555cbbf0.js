5d26285d7e2cc081492e2bfbe13fabe2c16c90a5
Luiz Filho <luizfilho.cont1@gmail.com>
2018-11-12 01:33:08 -0200

    Updates

---
+++ precache-manifest.96d691929df59a04c276e1c9555cbbf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07d20e6eb6bf9d5a7421",
+    "url": "/frontend-advagas/static/js/runtime~main.07d20e6e.js"
+  },
+  {
+    "revision": "3137f7ec3df7864b56bc",
+    "url": "/frontend-advagas/static/js/main.3137f7ec.chunk.js"
+  },
+  {
+    "revision": "f6398e2131237dc6861b",
+    "url": "/frontend-advagas/static/js/1.f6398e21.chunk.js"
+  },
+  {
+    "revision": "3137f7ec3df7864b56bc",
+    "url": "/frontend-advagas/static/css/main.d231f6ea.chunk.css"
+  },
+  {
+    "revision": "2b1418842857948ff47defcb87f1ba38",
+    "url": "/frontend-advagas/index.html"
+  }
+];
